c27be1d55daebbf463cd6c5841b286e15c3dc1b6
Omar Bencosme Tio <obencosmetio@gmail.com>
2026-01-29 23:59:20 -0400

    fix: corregir referencias a variables de entorno en la configuración de Drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
 
-import { validEnv } from './lib/env/config';
+import validEnv from './lib/env/config';
 
 export default defineConfig({
   out: './lib/db/migrations',
   schema: './lib/db/schema/index.ts',
   dialect: 'turso',
   dbCredentials: {
-    url: validEnv.tursoUrl,
-    authToken: validEnv.nodeEnv === 'development' ? undefined : validEnv.tursoAuth
+    url: validEnv.TURSO_DATABASE_URL,
+    authToken: validEnv.NODE_ENV === 'development' ? undefined : validEnv.TURSO_AUTH_TOKEN
   },
   casing: 'snake_case'
 });
